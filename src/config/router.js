42160e64ee7dd89018677de4168af117079441e0
David Paucar <soydavidpaucar@gmail.com>
2022-10-19 18:21:26 -0500

    :art: imported todo router

--- src/config/router.js
+++ src/config/router.js
@@ -1,5 +1,7 @@
 import indexRouter from '@/routes/index';
+import todoRouter from '@/routes/todo.router';
 
 export default function (app) {
   app.use('/', indexRouter);
+  app.use('/', todoRouter);
 }
