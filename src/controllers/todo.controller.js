e8c2b93adaf78b4129ab989b4e688b1454d680ba
David Paucar <soydavidpaucar@gmail.com>
2022-10-19 20:17:38 -0500

    :art: created update one and delete one task method
    created update one and delete one task method, added comments and improve response codes

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -9,7 +9,7 @@ import db from '@/database';
 export const getAll = async (req, res) => {
   try {
     const todos = await db.models.todo.findAll();
-    return res.json({ success: true, data: todos });
+    return res.status(200).json({ success: true, data: todos });
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
   }
@@ -29,12 +29,18 @@ export const createOne = async (req, res) => {
     const todo = await db.models.todo.create({
       name, description, author, isComplete,
     });
-    return res.json({ success: true, data: todo });
+    return res.status(201).json({ success: true, data: todo });
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
   }
 };
 
+/**
+ * GET /task/:id
+ * @param req
+ * @param res
+ * @returns {Promise<*>}
+ */
 export const getOne = async (req, res) => {
   try {
     const { id } = req.params;
@@ -42,7 +48,52 @@ export const getOne = async (req, res) => {
     if (!todo) {
       return res.status(404).json({ success: false, message: 'Todo not found' });
     }
-    return res.json({ success: true, data: todo });
+    return res.status(200).json({ success: true, data: todo });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+/**
+ * PUT /task/:id
+ * @param req
+ * @param res
+ * @returns {Promise<*>}
+ */
+export const updateOne = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      name, description, author, isComplete,
+    } = req.body;
+    const todo = await db.models.todo.findByPk(id);
+    if (!todo) {
+      return res.status(404).json({ success: false, message: 'Todo not found' });
+    }
+    await todo.update({
+      name, description, author, isComplete,
+    });
+    return res.status(200).json({ success: true, data: todo });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+/**
+ * DELETE /task/:id
+ * @param req
+ * @param res
+ * @returns {Promise<*>}
+ */
+export const deleteOne = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await db.models.todo.findByPk(id);
+    if (!todo) {
+      return res.status(404).json({ success: false, message: 'Todo not found' });
+    }
+    await todo.destroy();
+    return res.status(204);
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
   }
