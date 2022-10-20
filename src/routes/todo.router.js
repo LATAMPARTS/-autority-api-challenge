9e5ec61691adf1a2433b32bc89873490a5683b75
David Paucar <soydavidpaucar@gmail.com>
2022-10-19 20:18:09 -0500

    :art: added route task for update and delete operations

--- src/routes/todo.router.js
+++ src/routes/todo.router.js
@@ -7,5 +7,7 @@ const router = Router();
 router.get('/tasks', todoController.getAll);
 router.post('/task', todoController.createOne);
 router.get('/task/:id', todoController.getOne);
+router.put('/task/:id', todoController.updateOne);
+router.delete('/task/:id', todoController.deleteOne);
 
 export default router;
